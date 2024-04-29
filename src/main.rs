92f748df1eb97cd4dab7a7593cc86efd2e7edbae
Nuno Nebeker <nebeker@users.noreply.github.com>
2024-04-29 12:05:16 +0100

    Basic setup; server index.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::NamedFile;
+
+#[get("/")]
+async fn index() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("wwwroot/index.html").await
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
